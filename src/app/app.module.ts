d48f02800fe627229d7006513a5fbd15924f66be
atsa21 <ts21ann@gmail.com>
2023-03-04 21:03:09 +0200

    create structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
+import { HomePageComponent } from './components/main/home-page/home-page.component';
+import { ShoppingPageComponent } from './components/main/shopping-page/shopping-page.component';
+import { LoginComponent } from './components/main/login/login.component';
+import { SignUpComponent } from './components/main/sign-up/sign-up.component';
+import { SharedComponent } from './components/main/shared/shared.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HomePageComponent,
+    ShoppingPageComponent,
+    LoginComponent,
+    SignUpComponent,
+    SharedComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
