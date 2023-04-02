7fc146e0bd754a2de9795f595d2d23b86a9cca0a
atsa21 <ts21ann@gmail.com>
2023-04-02 17:36:22 +0300

    fixed snack bar style

--- src/app/components/shared/auth-guard/auth.guard.ts
+++ src/app/components/shared/auth-guard/auth.guard.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from 'src/app/services/auth-service/auth.service';
-import { SnackBarComponent } from '../snack-bar/snack-bar.component';
+import { SnackBarService } from 'src/app/services/snack-bar-service/snack-bar.service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +12,7 @@ export class AuthGuard implements CanActivate {
   constructor(
     private auth: AuthService,
     private router: Router,
-    private snack: SnackBarComponent
+    private snack: SnackBarService
   ) {}
 
   canActivate(
