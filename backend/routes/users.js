febc0639adde02457121cb93c0f56a71f9213764
atsa21 <ts21ann@gmail.com>
2023-04-03 18:50:48 +0300

    added error messages

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -15,12 +15,12 @@ router.post("/signup", (req, res, next) => {
             user.save()
             .then(result => {
                 res.status(201).json({
-                    message: "User was created!",
-                    result: result
+                  message: "User was created!",
+                  result: result
                 });
             }).catch(err => {
                 res.status(500).json({
-                    error: err
+                  message: "Invalid authentication credentials!"
                 });
             });
         });
@@ -59,7 +59,7 @@ router.post("/login", (req, res, next) => {
     })
     .catch(err => {
       return res.status(401).json({
-        message: "Auth failed"
+        message: "Invalid authentication credentials!"
       });
     });
 });
--- src/app/services/error-interceptor/error-interceptor.ts
+++ src/app/services/error-interceptor/error-interceptor.ts
@@ -1,17 +1,20 @@
 import { HttpErrorResponse, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, throwError } from 'rxjs';
+import { SnackBarService } from '../snack-bar-service/snack-bar.service';
 
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
+  constructor(private snack: SnackBarService) {}
+
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     return next.handle(req).pipe(
         catchError((error: HttpErrorResponse) => {
-            console.log(error);
-            alert(error.error.message);
-            return throwError(error);
+          const errorMessage = error.error.message ? error.error.message : 'An unknown error occured!';
+          this.snack.openSnackBar(errorMessage, 'error');
+          return throwError(error);
         })
     );
   }
