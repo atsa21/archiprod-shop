cbca99f2944e53ee7f0d5912773aac8231cbe60e
atsa21 <ts21ann@gmail.com>
2023-04-28 21:17:31 +0300

    brand update fix

--- src/app/components/shared/brand-card/brand-card.component.ts
+++ src/app/components/shared/brand-card/brand-card.component.ts
@@ -4,6 +4,7 @@ import { Brand } from 'src/app/models/products/brand.interface';
 import { BrandService } from 'src/app/services/brands-service/brand.service';
 import { SnackBarService } from 'src/app/services/snack-bar-service/snack-bar.service';
 import { AddEditBrandDialogComponent } from '../../admin/brands/add-edit-brand-dialog/add-edit-brand-dialog.component';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-brand-card',
@@ -30,11 +31,11 @@ export class BrandCardComponent {
     });
   }
 
-  deleteProduct(): void {
+  deleteBrand(): void {
     if (this.brand.id){
-      // this.brandService.deleteProduct(this.product.id).pipe(take(1)).subscribe((res) => {
-      //   this.snack.openSnackBar(res.message, 'success');
-      // })
+      this.brandService.deleteBrand(this.brand.id).pipe(take(1)).subscribe((res) => {
+        this.snack.openSnackBar(res.message, 'success');
+      })
     }
   }
 }
