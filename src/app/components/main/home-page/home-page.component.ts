694764a967d95fd334a7eee47b42eab15a2dbfe1
atsa21 <ts21ann@gmail.com>
2023-05-02 19:19:50 +0300

    added categories to homepage

--- src/app/components/main/home-page/home-page.component.ts
+++ src/app/components/main/home-page/home-page.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Subject, map, takeUntil } from 'rxjs';
+import { CategoryRes } from 'src/app/models/products/category-res.interface';
+import { Category } from 'src/app/models/products/category.interface';
 import { ProductCard, ProductListRes } from 'src/app/models/products/product-card.interface';
+import { CategoryService } from 'src/app/services/category-service/category.service';
 import { ProductService } from 'src/app/services/product-service/product.service';
 
 @Component({
@@ -10,6 +13,7 @@ import { ProductService } from 'src/app/services/product-service/product.service
 })
 export class HomePageComponent implements OnInit {
 
+  categories: Category[] = [];
   products: ProductCard[] = [];
   prodOnSale: ProductCard[] = [];
 
@@ -20,20 +24,22 @@ export class HomePageComponent implements OnInit {
     { id:'4', title: '8 Tips for choosing the Perfect Sofa', image:'/assets/img/homepage-what-is-arch.png' }
   ];
 
-  private destroy: Subject<boolean> = new Subject<boolean>();
+  private destroy$: Subject<boolean> = new Subject<boolean>();
 
   constructor(
-    private productService: ProductService
+    private productService: ProductService,
+    private categoryService: CategoryService
   ){}
 
   ngOnInit(): void {
     this.getProductsNotOnSale();
     this.getProductsOnSale();
+    this.getCategories();
   }
 
   private getProductsNotOnSale(): void {
     this.productService.getProductsOnSale(1, 4, false)
-    .pipe(takeUntil(this.destroy))
+    .pipe(takeUntil(this.destroy$))
     .subscribe((res: ProductListRes) => {
       const prodOnSaleList = res.data.map(el => ({ ...el, id: el._id })).map(({ _id, ...rest }) => rest);
       this.products = prodOnSaleList;
@@ -42,10 +48,26 @@ export class HomePageComponent implements OnInit {
 
   private getProductsOnSale(): void {
     this.productService.getProductsOnSale(1, 4, true)
-    .pipe(takeUntil(this.destroy))
+    .pipe(takeUntil(this.destroy$))
     .subscribe((res: ProductListRes) => {
       const prodOnSaleList = res.data.map(el => ({ ...el, id: el._id })).map(({ _id, ...rest }) => rest);
       this.prodOnSale = prodOnSaleList;
+    })
+  }
+
+  private getCategories(): void {
+    this.categoryService.getAllCategories()
+      .pipe(map((data) => {
+        return data.data.map( (res: CategoryRes) => {
+          return {
+            id: res._id,
+            name: res.name,
+            type: res.type
+          }
+        })
+      }), takeUntil(this.destroy$))
+      .subscribe(data => {
+        this.categories = data;
     })
   }
 }
