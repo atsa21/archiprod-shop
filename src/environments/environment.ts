12f8f0e136374c4511b4914b9ea6c9121fe7f85b
atsa21 <ts21ann@gmail.com>
2023-06-26 12:37:29 +0300

    changed env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: "http://archiprod-api-env.eba-g2depr4q.us-east-2.elasticbeanstalk.com/api"
+    apiUrl: "https://archiprod.glitch.me/api"
 };
